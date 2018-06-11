4f52132fc38e241adf65c41faa9461b0fbecde08
admins <sonpxvn@gmail.com>
2018-06-11 11:41:51 +0700

    first commit

---
+++ Boom/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Boom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Site:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(97, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(468, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(580, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "&START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(17, 151);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(265, 164);
+            this.listBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(265, 36);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "&Nhập Sock";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(289, 109);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(276, 36);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Nhập từ khóa";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(289, 152);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(276, 164);
+            this.listBox2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(635, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Tiến trình loading ...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "label3";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(17, 358);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(836, 184);
+            this.listBox3.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 324);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Logs";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(666, 20);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 45);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "STOP";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(752, 20);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(91, 45);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "CANCEL";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(572, 152);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(271, 192);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 554);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Boom/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Threading;
+using CefSharp.WinForms;
+using CefSharp;
+
+namespace Boom
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker workerThread = null; 
+        bool _keepRunning = false;
+        public ChromiumWebBrowser browser;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InstantiateWorkerThread();
+        }
+
+        private void InstantiateWorkerThread()
+        {
+            workerThread = new BackgroundWorker();
+            //workerThread.ProgressChanged += WorkerThread_ProgressChanged;
+            workerThread.DoWork += WorkerThread_DoWork;
+            //workerThread.RunWorkerCompleted += WorkerThread_RunWorkerCompleted;
+            workerThread.WorkerReportsProgress = true;
+            workerThread.WorkerSupportsCancellation = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "TXT files|*.txt";
+            theDialog.InitialDirectory = @"C:\";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+                int counter = 0;
+                string line;
+                string filename = theDialog.FileName;
+                System.IO.StreamReader file = new System.IO.StreamReader(filename);
+                while ((line = file.ReadLine()) != null)
+                {
+                    listBox1.Items.Add(line);
+                    counter++;
+                }
+                file.Close();
+                 
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "TXT files|*.txt";
+            theDialog.InitialDirectory = @"C:\";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                listBox2.Items.Clear();
+                int counter = 0;
+                string line;
+                string filename = theDialog.FileName;
+                System.IO.StreamReader file = new System.IO.StreamReader(filename);
+                while ((line = file.ReadLine()) != null)
+                {
+                    listBox2.Items.Add(line);
+                    counter++;
+                }
+                file.Close();
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            workerThread.RunWorkerAsync();
+        }
+
+
+        private void WorkerThread_DoWork(object sender, DoWorkEventArgs e)
+        {
+            DateTime startTime = DateTime.Now;
+
+            _keepRunning = true;
+
+            while (_keepRunning)
+            {
+                Thread.Sleep(500); 
+                Chilkat.Http http = new Chilkat.Http();
+                http.SocksVersion = 5;
+                int count_item = listBox2.Items.Count;
+                int count = 0;
+                for (int i = 0; i < listBox2.Items.Count; i++)
+                {
+                    
+                    count++;
+                    this.Invoke(new MethodInvoker(delegate ()
+                        {
+                            listBox2.SelectedIndex = i;
+                        }
+                    ));
+
+                    string line = listBox2.Items[i].ToString(); 
+                    this.Invoke(new MethodInvoker(delegate ()
+                    {
+                        label3.Text = "Đang từ khóa: " + line;
+                    }
+                    ));
+
+                    /** 
+                     * Bắt đầu chạy Sock 
+                     * */
+                    for (int z = 0; z < listBox1.Items.Count; z++)
+                    {
+                        if (browser != null && browser.IsBrowserInitialized)
+                        {
+                            browser.Dispose();
+                        }
+                        //listBox1.SelectedIndex = z;
+                        string keyword = listBox2.Items[z].ToString();
+                        InitBrowser(keyword, listBox1.Items[i].ToString());
+                         
+                    }
+                     
+                     
+                }
+
+
+                string timeElapsedInstring = (DateTime.Now - startTime).ToString(@"hh\:mm\:ss"); 
+                workerThread.ReportProgress(0, timeElapsedInstring); 
+                if (workerThread.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+            }
+        }
+
+        private void InitBrowser(string keyword)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            _keepRunning = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            workerThread.CancelAsync();
+        }
+
+
+        public void InitBrowser(string url, string sock5)
+        {
+
+           
+            CefSettings cfsettings = new CefSettings();
+            cfsettings.CefCommandLineArgs.Add("proxy-server", "socks5://" + sock5);
+            cfsettings.UserAgent = "My/Custom/User-Agent-AndStuff";
+            Cef.Initialize(cfsettings);
+
+            browser = new ChromiumWebBrowser(url);
+            this.Invoke(new MethodInvoker(delegate ()
+            {
+                groupBox1.Controls.Add((Control)browser);
+                browser.Dock = DockStyle.Fill;
+            }
+            ));
+            this.Invoke(new MethodInvoker(delegate ()
+            {
+                listBox3.Items.Add(url + "" + sock5);
+            }
+            ));
+            Thread.Sleep(1000);
+            try
+            {
+                browser.Dispose();
+                Cef.Shutdown();
+            }
+            catch (Exception ex)
+            {
+                listBox3.Items.Add("Unable to reinitialize" + ex);
+
+            }
+
+
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Cef.Shutdown();
+        }
+    }
+}
